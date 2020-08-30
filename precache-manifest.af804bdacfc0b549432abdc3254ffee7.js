1d203f661544b86ad95b548c5321a32c513ca31c
Zaid Sheriff <mszaid.007@gmail.com>
2020-08-30 16:13:25 +0530

    Updates

---
+++ precache-manifest.af804bdacfc0b549432abdc3254ffee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72687cb8c639fe07832369ef2c964ed",
+    "url": "/React-Canvas-Art/index.html"
+  },
+  {
+    "revision": "ef5d267e6250933e52e9",
+    "url": "/React-Canvas-Art/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72d2aaaa5fcb2ec7a682",
+    "url": "/React-Canvas-Art/static/js/2.a636e200.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Canvas-Art/static/js/2.a636e200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5d267e6250933e52e9",
+    "url": "/React-Canvas-Art/static/js/main.ef817ae7.chunk.js"
+  },
+  {
+    "revision": "0fa5ea03dc4155001e8f",
+    "url": "/React-Canvas-Art/static/js/runtime-main.15b40a10.js"
+  }
+]);
