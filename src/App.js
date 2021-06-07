40fbcfe0df8e8feddd09e763037c8875e0f67cc8
Zaid Sheriff <mszaid.007@gmail.com>
2021-06-07 11:02:00 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from "react"
 
+var fabric = "fabric.js"
 function App() {
-	const canvasRef = useRef(null)
-	const contextRef = useRef(null)
-	const [ isDrawing, setIsDrawing ] = useState(false)
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
 
-	useEffect(() => {
-		const canvas = canvasRef.current
-		canvas.width = window.innerWidth * 2
-		canvas.height = window.innerHeight * 2
-		canvas.style.width = `${window.innerWidth}px`
-		canvas.style.height = `${window.innerHeight}px`
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight * 2
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
 
-		const context = canvas.getContext('2d')
-		context.scale(2, 2)
-		context.lineCap = 'round'
-		context.strokeStyle = 'black'
-		context.lineWidth = 5
-		contextRef.current = context
-	}, [])
-	const startDrawing = ({ nativeEvent }) => {
-		const { offsetX, offsetY } = nativeEvent
-		contextRef.current.beginPath()
-		contextRef.current.moveTo(offsetX, offsetY)
-		setIsDrawing(true)
-	}
-	const finishDrawing = ({ nativeEvent }) => {
-		contextRef.current.closePath()
-		setIsDrawing(false)
-	}
-	const draw = ({ nativeEvent }) => {
-		if (!isDrawing) {
-			return
-		}
-		const { offsetX, offsetY } = nativeEvent
-		contextRef.current.lineTo(offsetX, offsetY)
-		contextRef.current.stroke()
-	}
-	return <canvas ref={canvasRef} onMouseDown={startDrawing} onMouseUp={finishDrawing} onMouseMove={draw} />
+    const context = canvas.getContext("2d")
+    context.scale(2, 2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context
+  }, [])
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+  const finishDrawing = ({ nativeEvent }) => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY } = nativeEvent
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+    />
+  )
 }
 
 export default App
